412eab62c5fec6914a694ad721521412b2dfb10e
Vishal <68778483+Vishaalcodre@users.noreply.github.com>
2022-12-22 17:22:51 +0000

    First Commit

--- main.py
+++ main.py
@@ -8,3 +8,59 @@
 # If they run out of turns, provide feedback to the player. 
 # Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
 
+from art import logo
+from random import randint
+
+def high_or_low(n, guess, chances):
+  if n > guess:
+    print("Too low")
+    return chances - 1
+    
+
+  elif n < guess:
+    print("Too high")
+    return chances - 1
+
+  else:
+    print(f"You got it right!, the answer is {n}")
+
+
+chances = 0
+
+
+
+def game():
+
+  print(logo)
+  print("Welcome to the Number Guessing Game!")
+  print("I'm thinking of a number between 1 and 100.")
+
+  N = randint(1,100)
+  
+  lvl = input("Choose a difficulty. Type 'easy' or 'hard': ")
+
+  if lvl == 'easy':
+    chances = 10
+
+  else:
+    chances = 5
+    
+  
+  guess = 0
+  
+  while guess != N:
+
+    print(f"You have {chances} attempts remaining to guess the number.\n")
+    
+    guess = int(input("Make a Guess: \n"))
+    
+    chances = high_or_low(N,guess,chances)
+
+    if chances == 0:
+      print("You've ran out of guesses, you lose.")
+      return
+
+    elif guess != N:
+      print("Guess again")
+
+game()
